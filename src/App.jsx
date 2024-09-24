9eae6ac6f2c9d2363fc36223329e6749cad05217
rajatanwar00 <apj.rajat@gmail.com>
2024-09-24 15:19:35 +0530

    changes to blue

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <div className="flex flex-col gap-4 w-20 bg-white p-2 rounded-xl border">
           <button className="border p-1 w-16 rounded-xl bg-red-600 text-white" onClick={()=>setColor("Red")}>Red</button>
           <button className="border p-1 w-16 rounded-xl bg-green-600 text-white" onClick={()=>setColor("Green")}>Green</button>
-          <button className="border p-1 w-16 rounded-xl bg-blue-600 text-white" onClick={()=>setColor("Blue")}>Blue</button>
+          <button className="border p-1 w-16 rounded-xl text-white" style={{ backgroundColor: "#1273de" }} onClick={()=>setColor("#1273de")}>Blue</button>
           <button className="border p-1 w-16 rounded-xl bg-black text-white" onClick={()=>setColor("Black")}>Black</button>
           <button className="border p-1 w-16 rounded-xl bg-yellow-600 text-white" onClick={()=>setColor("#ffc107")}>Yellow</button>
           <button className="border p-1 w-16 rounded-xl bg-violet-600 text-white" onClick={()=>setColor("#7b1fa2")}>Violet</button>
